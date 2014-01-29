844f147dd1b75450694f797ebbbc2d5324d1588d
max-konin <mk@kernel-corp.com>
2014-01-29 16:23:01 +0700

    assets fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Snet::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css.sass, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jobs.js graphs.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
