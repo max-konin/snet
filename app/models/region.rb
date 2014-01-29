bb2c6876ef27e3d2d80bf928d3eea94c8209b57a
max-konin <mk@kernel-corp.com>
2014-01-29 12:37:18 +0700

    calculate region centers

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,14 @@ class Region < ActiveRecord::Base
   belongs_to :job
   has_many :points, dependent: :destroy
   accepts_nested_attributes_for :points
+
+  def center
+    lat  = 0
+    long = 0
+    points.each do |p|
+      lat  += p.latitude
+      long += p.longitude
+    end
+    Point.new latitude: (lat / points.count), longitude: (long / points.count)
+  end
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Region do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'calculate center for square' do
+    region = Region.new
+    region.points.build(latitude: 0, longitude: 0)
+    region.points.build(latitude: 2, longitude: 2)
+    region.points.build(latitude: 0, longitude: 2)
+    region.points.build(latitude: 2, longitude: 0)
+    region.save
+
+    center = region.center
+    center.latitude.should eq(1)
+    center.longitude.should eq(1)
+  end
 end
